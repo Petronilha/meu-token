abe10f1299df24540bef5a61d7af25413cc91a37
Daniel <petronilhadaniel@gmail.com>
2025-12-14 00:25:57 -0300

    Creating contracts to mint 2 tokens

---
+++ contracts/TokenA.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.27;
+
+import "./ERC20.sol";
+
+contract TokenA is ERC20 {
+  constructor(string memory name, string memory symbol, uint8 decimals) ERC20(name, symbol, decimals) {
+    _mint(msg.sender, 100 * 10 ** uint256(decimals));
+  }
+}
+
